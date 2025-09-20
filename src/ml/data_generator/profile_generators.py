f68a7425b1a912abc485622910ef5801214acd3e
Yvonne Hong <yvonnehong2003@gmail.com>
2025-09-20 12:55:49 -0700

    added devnotes

--- src/ml/data_generator/profile_generators.py
+++ src/ml/data_generator/profile_generators.py
@@ -147,6 +147,10 @@ class CandidateProfileGenerator:
         matching_skills: List[str] = random.sample(all_job_skills, num_matching_skills)
         #^randomly selects num_matching_skills items from all_job_skills
 
+        """
+        START CHECKING FROM BELOW HERE AND GOING OVER COMPREHENSIVELY.
+        """
+
         #next: add some unrelated skills
         unrelated_skills = []
         for category in self.taxonomy.programming_languages.values(): 
